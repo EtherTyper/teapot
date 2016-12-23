5939d68e7bf59362acd51caa09a69dfb2cbe77c8
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2016-12-23 11:53:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<?php
+<?hh // strict
+
 header("I'm a teapot!", true, 418);
-echo '<!-- Yes, this is a joke. No, you cannot get an RFC 7168 compliant teapot. !-->';
-echo '<head><title>I\'m a teapot!</title></head>';
-echo '<body style="margin: 0"><iframe src="418.html" style="border: 0; width: 100%; height: 100%" /></body>';
-?>
+echo <html>
+  <!-- Yes, this is a joke. No, you cannot get an RFC 7168 compliant teapot. !-->;
+  <head>
+    <title>
+  { "I'm a teapot!" }
+    </title>
+  </head>
+  <body style="margin: 0">
+    <iframe src="418.html" style="border: 0; width: 100%; height: 100%" />
+    </body>;
+</html>;
